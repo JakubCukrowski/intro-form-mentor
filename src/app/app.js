ff373095794b4886f3f9fdb96c72c6e7ba148a63
JakubCukrowski <j.d.cukrowski@gmail.com>
2022-12-24 22:43:01 +0100

    Beta validation added

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,80 @@
 const form = document.querySelector("#form")
+const nameInput = form.querySelector("#firstName")
+const surnameInput = form.querySelector("#surname")
+const passInput = form.querySelector("#password")
+const emailInput = form.querySelector("#email")
+
+const nameValidation = () => {
+    const nameError = form.querySelector(".name-error")
+    const errorIcon = form.querySelector("#firstName").parentElement.nextElementSibling
+    if (nameInput.value.length === 0) {
+        nameError.style.display = "block"
+        nameInput.classList.add("invalid")
+        return errorIcon.style.display = "block"
+    }
+    nameError.style.display = "none"
+    nameInput.classList.remove("invalid")
+    errorIcon.style.display = "none"
+}
+
+const surnameValidation = () => {
+    const surnameError = form.querySelector(".surname-error")
+    const errorIcon = form.querySelector("#surname").parentElement.nextElementSibling
+    if (surnameInput.value.length === 0) {
+        surnameError.style.display = "block"
+        surnameInput.classList.add("invalid")
+        return errorIcon.style.display = "block"
+
+    }
+    surnameError.style.display = "none"
+    surnameInput.classList.remove("invalid")
+    errorIcon.style.display = "none"
+}
+
+const emailValidation = () => {
+    const emailError = form.querySelector(".email-error")
+    const errorIcon = form.querySelector("#email").parentElement.nextElementSibling
+    if (emailInput.value.length === 0) {
+        emailError.style.display = "block"
+        emailInput.classList.add("invalid")
+        return errorIcon.style.display = "block"
+
+    }
+    emailError.style.display = "none"
+    emailInput.classList.remove("invalid")
+    errorIcon.style.display = "none"
+}
+
+const passValidation = () => {
+    const passError = form.querySelector(".password-error")
+    const errorIcon = form.querySelector("#password").parentElement.nextElementSibling
+    if (passInput.value.length === 0) {
+        passError.style.display = "block"
+        passInput.classList.add("invalid")
+        return errorIcon.style.display = "block"
+
+    }
+    passError.style.display = "none"
+    passInput.classList.remove("invalid")
+    errorIcon.style.display = "none"
+}
+
+const validate = () => {
+    nameValidation()
+    surnameValidation()
+    emailValidation()
+    passValidation()
+}
 
 const handleSubmit = (e) => {
     e.preventDefault()
-    console.log("Submitted!")
+    if (!validate()) {
+        alert("Sent")
+    }
 }
 
 form.addEventListener("submit", handleSubmit)
+nameInput.addEventListener("keyup", nameValidation)
+surnameInput.addEventListener("keyup", surnameValidation)
+emailInput.addEventListener("keyup", emailValidation)
+passInput.addEventListener("keyup", passValidation)
